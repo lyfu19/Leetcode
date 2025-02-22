2ca3eb920bd766452ac5bdba52753d1a1ffd5596
lyfu19 <403428724@qq.com>
2025-02-23 11:39:38 +1300

    1175. Prime Arrangements

--- Leetcode/main.swift
+++ Leetcode/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
+class Solution {
+    func numPrimeArrangements(_ n: Int) -> Int {
+        let MOD = 1_000_000_007
+        
+        func factorial(_ n: Int) -> Int {
+            guard n > 1 else {
+                return 1
+            }
+            return (n * factorial(n - 1)) % MOD
+        }
+        
+        let primeNumbers: Set<Int> = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                                 31, 37, 41, 43, 47, 53, 59, 61, 67,
+                                 71, 73, 79, 83, 89, 97]
+        var count = 0
+        var indexPrimes = Array(repeating: 0, count: 101)
+        for i in indexPrimes.indices {
+            if primeNumbers.contains(i) {
+                count += 1
+            }
+            indexPrimes[i] = count
+        }
+        
+        let primes = indexPrimes[n]
+        let unprimes = n - primes
+        
+        let count1 = factorial(primes)
+        let count2 = factorial(unprimes)
+        
+        let ans = (count1 * count2) % MOD
+        
+        return ans
+    }
+}
+
+let result = Solution().numPrimeArrangements(100)
+print(result)
 
